0206a7cf0b79d8dd8c38f0255a4ae11c18099eb5
Malorn44 <Malorn44@gmail.com>
2023-06-08 17:08:31 -0400

    test grid

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
-// $(document).on("scroll", function(){
-//     if($(document).scrollTop() > 86)
-//     {
-//       $("#navbar").addClass("sticky");
-//       $("#nav-logo").addClass("color-white");
-//       $(".nav-custom-link").addClass("color-white");
-//       $(".navbar-btn").addClass("nav-btn-icon");
-//     }
-//     else
-//     {
-//         $("#navbar").removeClass("sticky");
-//         $("#nav-logo").removeClass("color-white");
-//         $(".nav-custom-link").removeClass("color-white");
-//         $(".navbar-btn").removeClass("nav-btn-icon");
-//     }
-// });
-
-// $('.toggle').click(function () {
-//     "use strict";
-//     console.log('test');
-//     $('nav ul').slideToggle();
-// });
-
-
-// $(".toggle").on('click', function(event){
-    
-//     console.log('test');
-//     //(... rest of your JS code)
-// });
+$(document).on("scroll", function(){
+    if($(document).scrollTop() > 86)
+    {
+      $("#navbar").addClass("sticky");
+    //   $("#nav-logo").addClass("color-white");
+    //   $(".nav-custom-link").addClass("color-white");
+    //   $(".navbar-btn").addClass("nav-btn-icon");
+    }
+    else
+    {
+        $("#navbar").removeClass("sticky");
+        // $("#nav-logo").removeClass("color-white");
+        // $(".nav-custom-link").removeClass("color-white");
+        // $(".navbar-btn").removeClass("nav-btn-icon");
+    }
+});
 
 
 $(document).on('click','.toggle',function(e) {
     //handler code here
     "use strict";
-    $('nav ul').slideToggle();
+    $('header ul').slideToggle();
   });
 
 $(window).resize(function () {
     "use strict";
-    if ($(window).width() > 780) {
-        $('nav ul').removeAttr('style');
+    if ($(window).width() > 500) {
+        $('header ul').removeAttr('style');
+        // $('header ul').slideToggle();
     }
 });
