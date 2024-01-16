fac4cbe112d05a0190bfec471b6bd63573b8a14c
Malorn44 <Malorn44@gmail.com>
2024-01-16 00:24:03 -0500

    dark theme beta

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// root element
+const root = document.documentElement;
+
 $(document).on("scroll", function(){
     if($(document).scrollTop() > 86)
     {
-      $("#navbar").addClass("sticky");
+        $("#navbar").addClass("sticky");
     //   $("#nav-logo").addClass("color-white");
     //   $(".nav-custom-link").addClass("color-white");
     //   $(".navbar-btn").addClass("nav-btn-icon");
@@ -39,5 +42,33 @@ $(window).resize(function () {
             $('header .hamburger').removeClass('change');   
             isClosed = true;
         }
+    }
+});
+
+// Dark Theme
+// when the document is ready
+$(document).ready(function () {
+    // Get the user's preference from localStorage
+    const darkMode = localStorage.getItem("dark-mode");
+    // Check if the user has already chosen a theme
+    if (darkMode) {
+        // If yes, apply it to the root element
+        root.classList.add("dark-theme");
+        $('.theme-toggle > span').removeClass('fa-moon');
+        $('.theme-toggle > span').addClass('fa-sun');
+    }
+});
+
+$(document).on('click','.theme-toggle',function(e) {
+    root.classList.toggle("dark-theme");
+    // Store or remove the user's preference in localStorage
+    if (root.classList.contains("dark-theme")) {
+        localStorage.setItem("dark-mode", true);
+        $('.theme-toggle > span').removeClass('fa-moon');
+        $('.theme-toggle > span').addClass('fa-sun');
+    } else {
+        $('.theme-toggle > span').removeClass('fa-sun');
+        $('.theme-toggle > span').addClass('fa-moon');
+        localStorage.removeItem("dark-mode");
     }
 });
